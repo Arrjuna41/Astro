bae2fc0e84caca1ad1f94e70ec51976254446d04
Arrjuna P <105677807+ooantavaoo@users.noreply.github.com>
2026-01-09 17:36:12 -0500

    Update H-R_Diagram.py
    01/09/26
    
    Created the equations for black body energy emission for the overall star.

--- H-R_Diagram.py
+++ H-R_Diagram.py
@@ -5,14 +5,18 @@ import matplotlib.pyplot as mat
 #Range of Temperatures: 2,500 Kelvin - 50,000 Kelvin
 #Range of Luminosity: 10^-4 solar units - 10^6 solar units
 
-rand_temp = Np.random.rand(2,500, 50001)
+solar_radius = 6.957 * 10**8 # meters
+solar_kelvin = 5778 # Kelvin
+solar_lum = 3.828 * 10**26 # watts
+
+rand_temp = Np.random.rand(2500, 50001)
 rand_lum = Np.random.rand(10 ** -4, 10 ** 6)
+rand_radius = Np.random.rand(0.01 * solar_kelvin, 3000 * solar_kelvin)
+
+
 
 σ = 5.67 * 10 ** -8 # W/m^2/K^4, Approximation of Stefan - Boltzmann Constant
 
-lum_list = Np.arange(40, 40)
-
-temp_list = None
 
 
 def fill_array(rows, cols, small, large):
@@ -24,8 +28,13 @@ def fill_array(rows, cols, small, large):
             arr[r][c] = Np.random.uniform(small, large + 1)
 
     return (arr)    
+# Luminosity = 4πr^2 * σT^4
 
-    
-           
+#Lum_solar_units = 4*Np.pi()*σ*
+
+lum_list = fill_array(40, 40, 2500, 50000)
+temp_list = fill_array(40, 40, 10**-4, 10**6)       
+
+
         
         
