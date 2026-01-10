0957f7068ed1740dac35a874bda7a45b10d89eef
Arrjuna P <105677807+ooantavaoo@users.noreply.github.com>
2026-01-09 22:05:07 -0500

    H_R Diagram Version 1
    01/09/26
    
    First rough attempt for an HR diagram. Got the basics of it, but the diagonal band is lacking. Still need to learn more on scattering and normal distribution to full hit the nail on the head with this one.

--- H-R_Diagram.py
+++ H-R_Diagram.py
@@ -1,40 +1,49 @@
 import numpy as Np
-import pandas as pan
 import matplotlib.pyplot as mat
 
 #Range of Temperatures: 2,500 Kelvin - 50,000 Kelvin
 #Range of Luminosity: 10^-4 solar units - 10^6 solar units
 
-solar_radius = 6.957 * 10**8 # meters
+solar_radius = 6.957 * (10**8) # meters
 solar_kelvin = 5778 # Kelvin
-solar_lum = 3.828 * 10**26 # watts
+solar_lum = 3.828 * (10**26) # watts
 
 rand_temp = Np.random.rand(2500, 50001)
-rand_lum = Np.random.rand(10 ** -4, 10 ** 6)
-rand_radius = Np.random.rand(0.01 * solar_kelvin, 3000 * solar_kelvin)
-
-
+rand_lum = Np.random.uniform(10 ** -4, 10 ** 6)
+rand_radius = Np.random.uniform(0.1 * solar_radius, 1000 * solar_radius)
 
 σ = 5.67 * 10 ** -8 # W/m^2/K^4, Approximation of Stefan - Boltzmann Constant
 
 
 
-def fill_array(rows, cols, small, large):
-    r=0
-    c=0
-    arr = Np.empty((rows,cols))
-    for r in range (rows):
-        for c in range (cols):
-            arr[r][c] = Np.random.uniform(small, large + 1)
 
-    return (arr)    
 # Luminosity = 4πr^2 * σT^4
 
 #Lum_solar_units = 4*Np.pi()*σ*
 
-lum_list = fill_array(40, 40, 2500, 50000)
-temp_list = fill_array(40, 40, 10**-4, 10**6)       
+rows = 1
+amount = 10000
+
+temp_list = Np.random.normal(10000, 3000, amount)
+temp_lsit = Np.clip(temp_list, 2500, 50001)
+
+radius = (temp_list / solar_kelvin) ** 0.7
+radius_list = radius * Np.random.uniform( 0.1 * solar_radius, 3 * solar_radius, amount)
+radius_list = Np.clip(radius_list, 0.08 * solar_radius, 30 * solar_radius)
+
+lum_list = Np.ones(amount)
+lum_list = (4 * Np.pi * radius_list**2 * σ * temp_list**4) * Np.random.normal(1.0, 0.35, amount)
 
 
-        
-        
+
+#mat.plot(Np.log10(lum_list / solar_lum), Np.log10(temp_list / solar_kelvin), linestyle = '', color = 'r', alpha = 0.5)
+mat.figure(figsize=(8, 8))
+mat.title('H-R Diagram')
+mat.xlabel('Temeperature (Kelvin)')
+mat.ylabel('Luminosity (W/m^2/K^4)')
+mat.xscale('log')
+mat.yscale('log')
+mat.scatter(Np.log10(lum_list / solar_lum), Np.log10(temp_list / solar_kelvin), c = temp_list, cmap='coolwarm')
+mat.gca().invert_xaxis()
+mat.grid(True)
+mat.show() 
